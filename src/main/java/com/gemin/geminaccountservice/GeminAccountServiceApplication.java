690c779a93c263f3d56c9f828e753930ea58a19c
zurum <zurumogbonda@gmail.com>
2022-06-14 14:28:50 +0100

    initial commit

---
+++ src/main/java/com/gemin/geminaccountservice/GeminAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gemin.geminaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemin/geminaccountservice/GeminAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemin.geminaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
