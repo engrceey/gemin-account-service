eb503a5fa315bc49455cb95a852014dc9512929e
zurum <zurumogbonda@gmail.com>
2022-06-15 23:28:11 +0100

    add service test

---
+++ src/test/java/com/gemin/geminaccountservice/service/TransactionServiceTest.java
@@ -0,0 +1,80 @@
+package com.gemin.geminaccountservice.service;
+
+import com.gemin.geminaccountservice.dto.request.DepositAccountRequestDto;
+import com.gemin.geminaccountservice.dto.response.DepositResponseDto;
+import com.gemin.geminaccountservice.dto.response.PaginatedResponse;
+import com.gemin.geminaccountservice.entity.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+class TransactionServiceTest {
+
+    @Autowired
+    private TransactionService transactionService;
+    private DepositAccountRequestDto depositAccountRequestDto;
+    private DepositResponseDto depositResponseDto;
+
+
+    @BeforeEach
+    void setUp() {
+        depositAccountRequestDto = DepositAccountRequestDto.builder().build();
+        depositResponseDto = DepositResponseDto.builder().build();
+    }
+
+    @Test
+    @DisplayName("Deposit Fund Service Test")
+    void depositFunds() {
+        //GIVEN ::
+        depositAccountRequestDto = DepositAccountRequestDto
+                .builder()
+                .receiver("mr gemini")
+                .sender("mr gemini")
+                .amount(BigDecimal.valueOf(100.0))
+                .receiverAccountNumber(123456700L)
+                .build();
+
+        //WHEN
+        depositResponseDto = transactionService.depositFunds(depositAccountRequestDto);
+
+        //THEN
+        assertEquals("mr gemini", depositResponseDto.getReceiverName());
+        assertEquals(BigDecimal.valueOf(100.0), depositResponseDto.getAmount());
+        assertThat(depositResponseDto.isTransactionSuccessful()).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Get Transaction Empty DB")
+    void getTransactionsEmptyDB() {
+
+        //WHEN
+        PaginatedResponse<Transaction> transactions = transactionService.getTransactions(0, 5);
+
+        //THEN
+        assertThat(transactions.getContent().isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Get Transaction")
+    void getTransactions() {
+
+        //WHEN
+        depositFunds();
+        depositFunds();
+        depositFunds();
+        PaginatedResponse<Transaction> transactions = transactionService.getTransactions(0, 5);
+
+        //THEN
+        assertThat(!transactions.getContent().isEmpty()).isTrue();
+
+    }
+}
---
+++ src/test/java/com/gemin/geminaccountservice/service/UserServiceTest.java
@@ -0,0 +1,75 @@
+package com.gemin.geminaccountservice.service;
+
+import com.gemin.geminaccountservice.constants.enums.RegisterStatus;
+import com.gemin.geminaccountservice.dto.request.RegisterUserRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private RegisterUserRequestDto registerUserRequestDto;
+
+    @BeforeEach
+    void setUp() {
+        registerUserRequestDto = RegisterUserRequestDto.builder().build();
+    }
+
+    @Test
+    @DisplayName("Register user with zero initial credit")
+    void registerUserWithZeroInitialCredit() {
+        //GIVEN ::
+        registerUserRequestDto = RegisterUserRequestDto
+                .builder()
+                .initialCredit(BigDecimal.valueOf(0.0))
+                .firstName("redhat")
+                .lastName("java")
+                .password("1234567")
+                .confirmPassword("1234567")
+                .phoneNumber("08012345678")
+                .email("gemini@gmail.com")
+                .build();
+//
+        //WHEN
+        String response = userService.registerUser(registerUserRequestDto);
+
+        //THEN
+        assertEquals(RegisterStatus.SUCCESS.getRegistrationStatus(), response);
+
+
+    }
+
+    @Test
+    @DisplayName("Register user with initial credit")
+    void registerUserWithInitialCredit() {
+//GIVEN ::
+        registerUserRequestDto = RegisterUserRequestDto
+                .builder()
+                .initialCredit(BigDecimal.valueOf(10.0))
+                .firstName("redhat")
+                .lastName("java")
+                .password("1234567")
+                .confirmPassword("1234567")
+                .phoneNumber("08012345678")
+                .email("gemini@gmail.com")
+                .build();
+
+
+        //WHEN
+        String response = userService.registerUser(registerUserRequestDto);
+
+        //THEN
+        assertEquals("Registration with transaction successful", response);
+
+
+    }
+}
