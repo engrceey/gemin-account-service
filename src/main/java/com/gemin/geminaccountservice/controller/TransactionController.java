9abe3a20465b033d239e0b2b9463f8ab2ead2d72
zurum <zurumogbonda@gmail.com>
2022-06-15 15:48:39 +0100

    add user/trans endpoint

---
+++ src/main/java/com/gemin/geminaccountservice/controller/TransactionController.java
@@ -0,0 +1,58 @@
+package com.gemin.geminaccountservice.controller;
+
+
+import com.gemin.geminaccountservice.dto.request.DepositAccountRequestDto;
+import com.gemin.geminaccountservice.dto.response.ApiResponse;
+import com.gemin.geminaccountservice.dto.response.DepositResponseDto;
+import com.gemin.geminaccountservice.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping(path = "/deposit-funds")
+    public ResponseEntity<ApiResponse<DepositResponseDto>> depositFund(@RequestBody @Valid final DepositAccountRequestDto depositRequestDto) {
+        log.info("initiate endpoint to deposit fund [{}] ::", depositRequestDto.getReceiver());
+
+        DepositResponseDto response = transactionService.depositFunds(depositRequestDto);
+
+        return ResponseEntity.ok(ApiResponse.<DepositResponseDto>builder()
+                .isSuccessful(true)
+                .statusMessage("funds deposited successfully")
+                .data(response)
+                .build()
+        );
+    }
+
+//    @GetMapping(path = "transactions", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<ApiResponse<PaginatedResponse<Transaction>>> getAllTransaction(
+//            @RequestParam(required = false, defaultValue = "0") int start,
+//            @RequestParam(required = false, defaultValue = "5") int limit
+//    ) {
+//
+//        log.info("Transaction getAllTransaction - fetching all transactions");
+//        PaginatedResponse<Transaction> response = transactionService.getTransactions(start, limit);
+//
+//        return ResponseEntity.ok().body(ApiResponse.<PaginatedResponse<Transaction>>builder()
+//                .isSuccessful(true)
+//                .statusMessage("Fetched Transaction")
+//                .data(response)
+//                .build()
+//        );
+//    }
+
+
+}
